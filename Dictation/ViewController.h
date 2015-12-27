5ad585ef367aa8d0655e27e51fe808d5a4d735ca
Crystalcxd <547339930@qq.com>
2015-12-27 22:37:15 +0800

    Initial Commit

---
+++ Dictation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dictation
+//
+//  Created by Michael on 15/12/27.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
